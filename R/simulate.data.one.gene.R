7e3dc9e2b5429606d6b2db166604d510532a2ad8
ozway <lbrown60@vols.utk.edu>
2014-12-10 11:10:07 -0500

    Logan fix for codon not being updated

--- R/simulate.data.one.gene.R
+++ R/simulate.data.one.gene.R
@@ -4,6 +4,8 @@ simulate.data.one.gene <- function(gene.list,
                                    MES,GMT,BIS,
                                    simulation.type){
   
+  cat(paste("\tSimulating", gene.list$name, ". . .\n"))
+  
   gene.list$gene.dat$c_index[-length(gene.list$gene.dat$c_index)]=
     simulate.sequence(phi=gene.list$phi.value,
                       c_index=gene.list$gene.dat$c_index,
@@ -16,5 +18,17 @@ simulate.data.one.gene <- function(gene.list,
                       A.2 = genome.parms$A2,
                       Ne = genome.parms$Ne,
                       Q = genome.parms$Q)
+  
+  ## Update gene.dat$codon based on updated codon indices
+  codon.vector <- character(length = length(gene.list$gene.dat$codon))
+    #The stop codon has c_index 99, which doesn't correspond to codon.params$codon, so it is copied over directly.
+    #This is bad, and I intend to change it
+  codon.vector[length(codon.vector)] <- as.character(gene.list$gene.dat$codon[length(gene.list$gene.dat$codon)])
+  
+  for(ii in 1:(length(gene.list$gene.dat$codon)-1)){
+    codon.vector[ii] <- codon.params$codon[ which(codon.params$c_index == gene.list$gene.dat$c_index[ii]) ]
+  }
+  gene.list$gene.dat$codon <- factor(codon.vector)    #$codon needs to be a factor, not a char vector
   gene.list
+
 }
